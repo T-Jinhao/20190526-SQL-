112166a5a17be884a87cccb08b76acbacbdce0c3
挺无聊的 <1184028304@qq.com>
2019-09-26 20:37:28 +0800

    更新

--- mysqli.php
+++ mysqli.php
@@ -5,9 +5,9 @@
 $servername='localhost:3306';
 $username='';  #自行修改
 $password='';  #自行修改
-$database='supersqli';
+$database='supersqli';   #不用修改
 
-
+$con=mysqli_connect($servername,$username,$password)or die("数据库连接错误".mysqli_connect_error());
 $conn=mysqli_connect($servername,$username,$password,$database)or die("数据库连接错误".mysqli_connect_error());
-
 ?>
+
